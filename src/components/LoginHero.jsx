a9d842eae3b63215b81f952275fefd9f13056542
Abdul Rehman Khan <abdulrehman3725@gmail.com>
2024-07-11 13:49:07 +0500

    added headings

--- src/components/LoginHero.jsx
+++ src/components/LoginHero.jsx
@@ -10,9 +10,13 @@ function Hero({handleLogIn}) {
       <Wrapper>
         <Card>
           <FlexColumnCenter>
-            <h1>Open Sauced Dashboard</h1>
+            {/* <h1>Open Sauced Dashboard</h1>
             <p>Login with GitHub to start tracking open source projects.</p>
-            <br />
+            <br /> */}
+            <h1>The path to your next</h1>
+            <h2>Open Source</h2>
+            <h1>contribution and more pizza.</h1>
+
             <FlexCenter>
             <MarketingButton alt="pizza" primary onClick={handleLogIn}>
               Login with GitHub
