de3ae6fda7015ff5d6c2d16a9845c08afddaf98d
Abdul Rehman Khan <abdulrehman3725@gmail.com>
2024-07-11 13:56:09 +0500

    small space

--- src/components/LoginHero.jsx
+++ src/components/LoginHero.jsx
@@ -13,6 +13,7 @@ function Hero({handleLogIn}) {
             {/* <h1>Open Sauced Dashboard</h1>
             <p>Login with GitHub to start tracking open source projects.</p>
             <br /> */}
+            
             <h1>The path to your next</h1>
             <h2>Open Source</h2>
             <h1>contribution and more pizza.</h1>
